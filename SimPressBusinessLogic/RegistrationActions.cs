dc853ffd3e36b9f4b595fc9967242dc078e82214
Alexandr Korolchuk <alkor1@yandex.ru>
2012-09-19 20:55:38 +0300

    Begin to write busness logic
    some untested methods for registration

---
+++ SimPressBusinessLogic/RegistrationActions.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using SimPressDomainModel.EFDbContext;
+using SimPressDomainModel.Interfaces;
+using SimPressDomainModel.RepositoryClasses;
+
+namespace SimPressBusinessLogic
+{
+    /// <summary>
+    /// Some methods for Registration process: all checks and so on.
+    /// </summary>
+    public static class RegistrationActions
+    {
+        private static IRepository repository;
+        private static readonly string loginRegex = @"[A-Za-z][A-Za-z0-9_.]{3,31}";
+        static RegistrationActions()
+        {
+            //Rebuild using ninject
+            repository = new DBRepository(new SimPressEFDBContext(@"Data Source=ARTAL-ПК\SQLEXPRESS;Initial Catalog=SimPressDataBase;Integrated Security=True;Pooling=False"));
+        }
+        public static bool IsLoginFree(string login)
+        {
+            var user = repository.Users.FirstOrDefault(x => x.Login == login);
+            if (user == null)
+            {
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        
+        public  static bool IsLoginIsCorrected(string login)
+        {
+            return Regex.IsMatch(login, loginRegex);
+        }
+
+        public static bool IsLoginIsGood(string login)
+        {
+            return IsLoginFree(login) && IsLoginIsCorrected(login);
+        }
+    }
+}
