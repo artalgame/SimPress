9631f6afbbb84a42622c6632f1625261346eee70
ArtAlGame <alkor1@yandex.ru>
2012-09-20 09:37:00 +0300

    Unusefull commit
    sdfsdfdf

--- SimPressBusinessLogic/RegistrationActions.cs
+++ SimPressBusinessLogic/RegistrationActions.cs
@@ -6,42 +6,57 @@ using System.Text.RegularExpressions;
 using SimPressDomainModel.EFDbContext;
 using SimPressDomainModel.Interfaces;
 using SimPressDomainModel.RepositoryClasses;
+using Ninject;
+using Moq;
 
 namespace SimPressBusinessLogic
 {
     /// <summary>
     /// Some methods for Registration process: all checks and so on.
     /// </summary>
-    public static class RegistrationActions
+    public class RegistrationActions
     {
-        private static IRepository repository;
-        private static readonly string loginRegex = @"[A-Za-z][A-Za-z0-9_.]{3,31}";
+        private static readonly string CONNECTION_STRING = @"Data Source=ARTAL-ПК\SQLEXPRESS;Initial Catalog=SimPressDataBase;Integrated Security=True;Pooling=False";
+        private static readonly string LOGIN_REGEX = @"[A-Za-z][A-Za-z0-9_.]{3,31}";
+        private static IKernel ninjectKernel;
+
         static RegistrationActions()
         {
-            //Rebuild using ninject
-            repository = new DBRepository(new SimPressEFDBContext(@"Data Source=ARTAL-ПК\SQLEXPRESS;Initial Catalog=SimPressDataBase;Integrated Security=True;Pooling=False"));
-        }
-        public static bool IsLoginFree(string login)
-        {
-            var user = repository.Users.FirstOrDefault(x => x.Login == login);
-            if (user == null)
-            {
-                return true;
-            }
-            else
-            {
-                return false;
-            }
-        }
-        
-        public  static bool IsLoginIsCorrected(string login)
-        {
-            return Regex.IsMatch(login, loginRegex);
+            ninjectKernel = new StandardKernel();
+            ninjectKernel.Bind<SimPressEFDBContext>().ToSelf().WithConstructorArgument("connectionString", CONNECTION_STRING);
         }
 
-        public static bool IsLoginIsGood(string login)
+        private static SimPressEFDBContext GetContext()
+        {
+            return new SimPressEFDBContext(CONNECTION_STRING);
+        }
+
+        public bool IsLoginFree(string login)
+        {
+            using (IRepository repository = ninjectKernel.Get<DBRepository>())
+            {
+                var user = repository.Users.FirstOrDefault(x => x.Login == login);
+                if (user == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool IsLoginIsCorrected(string login)
+        {
+            return Regex.IsMatch(login, LOGIN_REGEX);
+        }
+
+        public bool IsLoginIsGood(string login)
         {
             return IsLoginFree(login) && IsLoginIsCorrected(login);
         }
+
+
     }
 }
