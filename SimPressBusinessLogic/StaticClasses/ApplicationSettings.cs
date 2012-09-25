231fa5260bf538c68a3f2b11155c757b8cb605af
ArtAlGame <alkor1@yandex.ru>
2012-09-25 07:27:45 +0300

    applicationSettingsAdded

---
+++ SimPressBusinessLogic/StaticClasses/ApplicationSettings.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using SimPressDomainModel.EFDbContext;
+using SimPressDomainModel.RepositoryClasses;
+
+namespace SimPressBusinessLogic.StaticClasses
+{
+    /// <summary>
+    /// This class realize general settings and basic mechanism of bussiness logic
+    /// </summary>
+    public static class ApplicationSettings
+    {
+        private static readonly string CONNECTION_STRING = @"Data Source=ARTAL-ПК\SQLEXPRESS;Initial Catalog=SimPressDataBase;Integrated Security=True;Pooling=False";
+        
+        public static readonly string LOGIN_REGEX = @"[A-Za-z][A-Za-z0-9_.]{3,31}";
+        public static readonly string PASSWORD_REGEX = @"^.*(?=.{6,24})(?=.*\d)(?=.*[a-zA-Z])$";
+        public static readonly string EMAIL_REGEX = @"^(?=.*\d)(?=.*[a-zA-Z]).{6,24}$";
+
+
+        private static SimPressEFDBContext GetContext()
+        {
+            return new SimPressEFDBContext(CONNECTION_STRING);
+        }
+
+        public  static SimPressDomainModel.Interfaces.IRepository GetRepository()
+        {
+            return new DBRepository(GetContext());
+        }
+    }
+}
---
+++ SimPressBusinessLogic/UsersActions/UserRegistration.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using SimPressDomainModel.Interfaces;
+using SimPressBusinessLogic.StaticClasses;
+
+namespace SimPressBusinessLogic
+{
+    /// <summary>
+    /// Some methods for Registration process: all checks and so on.
+    /// </summary>
+    public class UserRegistration
+    {
+        public UserRegistration()
+        {
+        }
+
+        public bool IsLoginFree(string login)
+        {
+            using (IRepository repository = ApplicationSettings.GetRepository())
+            {
+                var user = repository.Users.FirstOrDefault(x => x.Login == login);
+                if (user == null)
+                {
+                    return true;
+                }
+                else
+                {
+                    return false;
+                }
+            }
+        }
+
+        public bool IsLoginIsCorrected(string login)
+        {
+            return Regex.IsMatch(login, ApplicationSettings.LOGIN_REGEX);
+        }
+
+        public bool IsLoginIsGood(string login)
+        {
+            return IsLoginFree(login) && IsLoginIsCorrected(login);
+        }
+
+
+
+    }
+}
